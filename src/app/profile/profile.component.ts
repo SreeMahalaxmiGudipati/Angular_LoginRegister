cf69b941fa1a334a9f28f52363dba885da2285c1
mahalaxmi gudipati <mahalaxmigudipati@gmail.com>
2023-02-26 21:06:27 +0530

    displaying profile info

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UserprofileService } from '../userprofile.service';
 
 @Component({
   selector: 'app-profile',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class ProfileComponent {
 
+  users:any;
+constructor(private userprofile:UserprofileService){}
+
+ngOnInit():void{
+  this.users=this.userprofile.users;
+}
 }
