1a45162a629c9b8ece6fc67a19e6f4066ba0b4ee
mahalaxmi gudipati <mahalaxmigudipati@gmail.com>
2023-02-26 20:23:22 +0530

    creating objects

---
+++ src/app/userprofile.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserprofileService {
+
+  public users=[
+    {
+      id:1,
+      user:'ram',
+      pass:'1234',
+      phone:'8976459767',
+      address:'hyd'
+    },
+    {
+      id:2,
+      user:'laxman',
+      pass:'1234',
+      phone:'8976459767',
+      address:'hyd'
+
+    }
+  ]
+
+  constructor() { }
+
+ 
+}
