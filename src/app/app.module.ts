2a8680587f42cac61cf84c66acb2b40524156fb5
mahalaxmi gudipati <mahalaxmigudipati@gmail.com>
2023-02-26 19:03:12 +0530

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import {RouterModule, Routes} from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
 const appRoute : Routes=[
+  {path:'',component:HomeComponent},
   {path:'home',component:HomeComponent},
   {path:'login',component:LoginComponent},
   {path:'register',component:RegisterComponent}
