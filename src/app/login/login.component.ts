00b0d25185ea063130ac193566daad506c45e9fa
mahalaxmi gudipati <mahalaxmigudipati@gmail.com>
2023-02-26 20:38:57 +0530

    injecting serive method to login component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { UserprofileService } from '../userprofile.service';
 
 @Component({
   selector: 'app-login',
@@ -8,7 +9,7 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent {
 
-  constructor(private router: Router){
+  constructor(private userprofile:UserprofileService,private router:Router){
   }
   
 user:any
@@ -16,13 +17,22 @@ pass:any
 
 login()
 {
-  if(this.user=='admin'&& this.pass=='1234'){
+  // if(this.user=='admin'&& this.pass=='1234'){
+  //   alert("Login success");
+  //   this.router.navigate(['profile']);
+  // }
+  // else{
+  //   alert("Login fail");
+  // }
+  const users = this.userprofile.getUsers();
+  const user = users.find(u => u.user === this.user && u.pass === this.pass);
+  if (user) {
     alert("Login success");
-    this.router.navigate(['profile']);
+   
+  } else {
+   alert("Invalid username and password");
   }
-  else{
-    alert("Login fail");
-  }
+
 }
 }
 
