b8dc510267128efe32a9b13cfff59a83c96b0188
mahalaxmi gudipati <mahalaxmigudipati@gmail.com>
2023-02-28 12:05:21 +0530

    adding userservice

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { Form, FormControl,FormGroup} from '@angular/forms';
+import { UserService } from '../shared/user.service';
+
 
 @Component({
   selector: 'app-register',
@@ -8,6 +10,40 @@ import { FormGroup } from '@angular/forms';
 })
 export class RegisterComponent {
 
-
+  displayMsg:string='';
+  isAccountCreated: boolean=false;
   
+constructor(private userservice:UserService){}
+ 
+public registerForm =new FormGroup({
+  Name:new FormControl(''),
+  Phone:new FormControl(),
+  pass:new FormControl('')
+});
+  
+
+registerSubmitted(){
+    
+  this.userservice.registerUser([
+    this.registerForm.value.Name,
+    this.registerForm.value.Phone,
+    this.registerForm.value.pass
+]).subscribe(res=>{
+  if(res=='Success'){
+    this.displayMsg='Account created successfully';
+    this.isAccountCreated=true;
+  }
+  else {
+    this.displayMsg='Something wrong';
+    this.isAccountCreated=false;
+  }
+
+    })
+   
+ }
+
+
+
+ 
+
 }
