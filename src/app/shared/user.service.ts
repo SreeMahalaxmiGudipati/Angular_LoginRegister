98be9942e4693db66d02fef86a8df973da8eb077
mahalaxmi gudipati <mahalaxmigudipati@gmail.com>
2023-02-28 12:07:24 +0530

    adding http instance

---
+++ src/app/shared/user.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  private baseUrl ="https://localhost:7068/api/";
+  constructor(private http:HttpClient) { }
+
+  registerUser(user:Array<String>)
+  {
+    return this.http.post(this.baseUrl + 'Students' ,{
+      Name:user[0],
+      Phone:user[1],
+      Password:user[2]
+    },
+    {
+      responseType:'text',
+    });
+  }
+
+}
